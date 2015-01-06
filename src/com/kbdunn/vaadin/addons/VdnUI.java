8b6390927a2e73a6abf8c5591a4cc5e1fc708212
Bryson Dunn <k.bryson.dunn@gmail.com>
2015-01-05 17:11:35 -0700

    Minor Layout Changes

--- src/com/kbdunn/vaadin/addons/VdnUI.java
+++ src/com/kbdunn/vaadin/addons/VdnUI.java
@@ -4,6 +4,7 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -14,26 +15,32 @@ public class VdnUI extends UI  {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private VerticalLayout content;
+	private VerticalLayout layout;
 	private TabSheet tabsheet;
 	
 	@Override
 	protected void init(VaadinRequest request) {
-		content = new VerticalLayout();
+		setSizeFull();
+		Panel content = new Panel();
+		content.setSizeFull();
 		setContent(content);
-		content.addStyleName(ValoTheme.LAYOUT_WELL);
-		content.setMargin(true);
-		content.setSpacing(true);
 		
-		String sbody = "";
-		sbody += "<span style='font-size: 22px'>Welcome to Bryson's Addon Demo App. Use the tabs below to browse addons.</span>";
+		layout = new VerticalLayout();
+		content.setContent(layout);
+		layout.addStyleName(ValoTheme.LAYOUT_WELL);
+		layout.setMargin(true);
+		layout.setSpacing(true);
+		
+		String sbody = "<h2 style='margin:0 30px;'>Welcome to Bryson's Addon Demo App!</h2>";
+		sbody += "<p style='font-size: 16px; margin: 10px 30px;'>Use the tabs below to browse addons. "
+				+ "You can find the source code for this demo on GitHub <a href='https://github.com/bdunn44/Vaadin-Addon-Demo'>here</a>.</p>";
 		Label body = new Label(sbody, ContentMode.HTML);
-		content.addComponent(body);
+		layout.addComponent(body);
 		
 		tabsheet = new TabSheet();
 		tabsheet.addStyleName(ValoTheme.TABSHEET_FRAMED);
 		tabsheet.setSizeFull();
-		content.addComponent(tabsheet);
+		layout.addComponent(tabsheet);
 		
 		tabsheet.addTab(new FontAwesomeAddonLayout(), "Font Awesome Label");
 		tabsheet.addTab(new MejsAddonLayout(), "MediaElement.js Player");
@@ -46,9 +53,9 @@ public class VdnUI extends UI  {
 			footer += "<li><a href='http://www.linkedin.com/in/kbrysondunn/'>Bryson on LinkedIn</a></li>";
 			footer += "<li><a href='http://cloudnimbus.org'>Nimbus Personal Cloud</a></li>";
 		footer += "</ul>";
-		content.addComponent(new Label(footer, ContentMode.HTML));
+		layout.addComponent(new Label(footer, ContentMode.HTML));
 		
-		if (request.getParameter("addon") != null) {
+		if (request.getParameter("a") != null) {
 			String addon = request.getParameter("addon");
 			if ("FontAwesomeLabel".equals(addon)) tabsheet.setSelectedTab(0);
 			else if ("MejsPlayer".equals(addon)) tabsheet.setSelectedTab(1);
